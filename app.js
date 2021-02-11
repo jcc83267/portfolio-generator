b3b12d7417b362d502ee232ac90c14afa8b25405
James Canlas <jamescanlasjr@yahoo.com>
2021-02-10 22:37:19 -0800

    added validate and if they want an about section

--- app.js
+++ app.js
@@ -16,17 +16,46 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!')
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your Github Username?'
+            message: 'Enter your Github Username?',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your github username!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
         },
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({confirmAbout}) => {
+                if(confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ])
     
@@ -45,12 +74,28 @@ const promptProject = portforlioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name of your project?'
+            message: 'What is your name of your project?',
+            validate: projectInput => {
+                if (projectInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name for your project!')
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project. (Required)'
+            message: 'Provide a description of the project. (Required)',
+            validate: descInput => {
+                if (descInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of the project!')
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -61,7 +106,15 @@ const promptProject = portforlioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to you project. (Required)'
+            message: 'Enter the GitHub link to you project. (Required)',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your link!')
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
