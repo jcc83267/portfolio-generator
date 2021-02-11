6b1f2117f796c6d9680dacfee75ac7baf93b37eb
James Canlas <jamescanlasjr@yahoo.com>
2021-02-10 22:25:11 -0800

    added more question and project promt

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { prompt } = require('inquirer');
 const inquirer = require('inquirer');
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
@@ -9,12 +10,88 @@ const inquirer = require('inquirer');
 
 //     console.log("Portfolio completed! Check out index.html to see the output!")
 // });
-inquirer
-    .prompt([
+
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your Github Username?'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
         }
     ])
-    .then(answers => console.log(answers));
+    
+}
+const promptProject = portforlioData => {
+    if (!portforlioData.projects) {
+        portforlioData.projects = [];
+    }
+    
+    console.log(`
+=====================
+  Add a New Project
+=====================
+`);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project. (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to you project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+    .then(projectData => {
+        portforlioData.projects.push(projectData)
+        if (projectData.confirmAddProject) {
+            return promptProject(portforlioData);
+        } else {
+            return portforlioData;
+        }
+    });
+}
+
+
+promptUser()
+    .then(promptProject)
+    .then(portforlioData => {
+        console.log(portforlioData);
+    });
+
+
+
